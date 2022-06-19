9e7f2733d8ce6526d299297e5f4c8a0076a0d7b4
Cmaan-R <robsonleandrox@hotmail.com>
2022-06-19 07:26:06 -0300

    Update

--- exercicios-javaFX/src/layout/AppLayout.java
+++ exercicios-javaFX/src/layout/AppLayout.java
@@ -12,6 +12,7 @@ public class AppLayout extends Application{
 		raiz = new TesteAnchorPane();
 		raiz = new TesteBorderPane();
 		raiz = new TesteFlowPane();
+		raiz = new TesteGridPane();
 
 		Scene principal = new Scene(raiz, 800 , 600);
 		primaryStage.setScene(principal);

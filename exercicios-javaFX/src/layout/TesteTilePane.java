e244c5f4ad22c0f6645d40b37e5659469f2f0349
Cmaan-R <robsonleandrox@hotmail.com>
2022-06-21 17:19:14 -0300

    Update

--- exercicios-javaFX/src/layout/TesteTilePane.java
+++ exercicios-javaFX/src/layout/TesteTilePane.java
@@ -3,6 +3,7 @@ package layout;
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.layout.TilePane;
 
@@ -14,7 +15,11 @@ public class TesteTilePane extends TilePane{
 		for (int i = 0; i < 10 ; i++) {
 			quadrados.add(new Quadrado(i * 10));
 		}
+	
+		setVgap(10);
+		setHgap(10);
 		
+		setOrientation(Orientation.VERTICAL);
 		setTileAlignment(Pos.BOTTOM_RIGHT);
 		
 		getChildren().addAll(quadrados);
